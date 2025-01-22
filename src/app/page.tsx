d0bdd7daba87b76acb5d73498d9ea3f81e5afd72
mr.scrpt <mr.scrpt@gmail.com>
2025-01-22 12:38:35 +0200

    feat: grid v0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import sTest from "./test.module.scss";
 export default function Home() {
   return (
-    <div className={sTest.test}>
+    <div className={sTest.page}>
       <div className={sTest.section__inner}>
         <div className={sTest.section__sidebar}>BAR</div>
         <div className={sTest.section__content}>CONTENT</div>
