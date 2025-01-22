885fd10894ff0e5565dec57e909e20e91aeca4b4
mr.scrpt <mr.scrpt@gmail.com>
2025-01-22 12:19:14 +0200

    feat: custom grid

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    prependData: `@use "./src/shared/style/_mixin.scss" as *;`,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import sTest from "./test.module.scss";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={sTest.test}>
+      <div className={sTest.section__inner}>
+        <div className={sTest.section__sidebar}>BAR</div>
+        <div className={sTest.section__content}>CONTENT</div>
+      </div>
+    </div>
+  );
 }
