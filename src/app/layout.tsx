0a011a923d78f7b53dbf79e174a275eaa202ef2c
mr.scrpt <mr.scrpt@gmail.com>
2025-01-22 11:27:00 +0200

    chore: stylelint ignore comment, global module and etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Montserrat } from "next/font/google";
+import sGlobal from "@/shared/style/global.module.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const MontserratSans = Montserrat({
+  variable: "--font-montserrat-sans",
   subsets: ["latin"],
 });
 
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" className={sGlobal.html}>
+      <body className={`${MontserratSans.variable}`}>{children}</body>
     </html>
   );
 }
