709d2d27b7f04358e5babaf980c2475ad108c00e
mr <mr.scrpt@gmail.com>
2025-04-07 11:35:37 +0300

    feat: concept section impl

--- src/widget/concept/ui/list/item.tsx
+++ src/widget/concept/ui/list/item.tsx
@@ -5,6 +5,9 @@ import { ConceptItem } from "../../domain/concept.type";
 
 import { useConceptItemAnimationClass } from "../vm/useConceptItemAnimationClass.model";
 import sItem from "./item.module.scss";
+import { useTranslations } from "next-intl";
+import { CONCEPT_NAMESPACE } from "../../domain/concept.dict";
+import { Title } from "@/shared/ui/title";
 
 interface ItemProps extends HTMLAttributes<HTMLDivElement> {
   idx: number;
@@ -14,6 +17,7 @@ interface ItemProps extends HTMLAttributes<HTMLDivElement> {
 export const Item: FC<ItemProps> = (props) => {
   const { className, item, idx } = props;
   const { title, description } = item;
+  const t = useTranslations(CONCEPT_NAMESPACE);
 
   const { orderAnimationClassName, contentAnimationClassName, toggleOpen } =
     useConceptItemAnimationClass();
@@ -29,8 +33,15 @@ export const Item: FC<ItemProps> = (props) => {
         </div>
         <div className={clsx(sItem.box, [contentAnimationClassName])}>
           <div className={clsx(sItem.content)}>
-            <div className={sItem.title}>{title}</div>
-            <div className={sItem.description}>{description}</div>
+            <Title
+              className={sItem.title}
+              text={t(title)}
+              size="S"
+              view="PRIMARY"
+              as="h3"
+              // onClick={toggleOpen}
+            />
+            <div className={sItem.description}>{t(description)}</div>
           </div>
         </div>
       </div>
