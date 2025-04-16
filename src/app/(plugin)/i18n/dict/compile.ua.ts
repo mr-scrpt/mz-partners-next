b2c2dfbc642dce4227a8fe5fe1b056f9e77fa9e6
mr <mr.scrpt@gmail.com>
2025-04-16 11:16:25 +0300

    feat: clear menu dict

--- src/app/(plugin)/i18n/dict/compile.ua.ts
+++ src/app/(plugin)/i18n/dict/compile.ua.ts
@@ -1,6 +1,5 @@
 import { CallBackDict } from "@/feature/callback";
 import { LangSwitcherDict } from "@/shared/ui/langSwitcher";
-// import { MenuDict } from "@/shared/ui/menu";
 import { validateMessages } from "../domain/namespace.type";
 import { HeroDict } from "@/widget/hero/domain/hero.dict";
 import { ConceptDict } from "@/widget/concept";
@@ -8,7 +7,6 @@ import { PrincipleDict } from "@/widget/principle";
 import { ProjectDict } from "@/widget/project";
 
 export const messages = validateMessages({
-  // ...MenuDict.ua,
   ...LangSwitcherDict.ua,
   ...HeroDict.ua,
   ...CallBackDict.ua,
