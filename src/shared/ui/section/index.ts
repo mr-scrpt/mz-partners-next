83f6b3eee78d94a4ce34267c3c69894f64f09543
mr <mr.scrpt@gmail.com>
2025-06-25 14:20:09 +0300

    fix: remove old layout section

--- src/shared/ui/section/index.ts
+++ src/shared/ui/section/index.ts
@@ -1,8 +1,4 @@
-export { FooterSection } from "./ui/footer.section";
-
-export { HeaderSection } from "./ui/header.section";
 export { RowSection } from "./ui/row.section";
-export { BoxSection } from "./ui/box.section";
 
 export { SECTION_SPACE } from "./domain/space.type";
 export { SECTION_WIDTH } from "./domain/topology.type";
