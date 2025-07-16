8374a8c60182b13cd307c5aa3b076f2f4fecd79a
mr <mr.scrpt@gmail.com>
2025-07-16 09:29:24 +0300

    fix: logo link with local

--- src/widget/footer/ui/hero/hero.footer.tsx
+++ src/widget/footer/ui/hero/hero.footer.tsx
@@ -4,15 +4,17 @@ import { FC, HTMLAttributes } from "react";
 import sHeroFooter from "./hero.footer.module.scss";
 import { SocialListFooter } from "../social/socialList.footer";
 import { LogoLink } from "@/shared/ui/logo/ui/logoLink";
+import { useLocale } from "next-intl";
 
 interface HeroFooterProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const HeroFooter: FC<HeroFooterProps> = (props) => {
+  const locale = useLocale();
   return (
     <div className={sHeroFooter.root}>
       <div className={sHeroFooter.inner}>
         <div className={sHeroFooter.logo}>
-          <LogoLink href="/" view="LIGHT" />
+          <LogoLink href={`/${locale}`} view="LIGHT" />
         </div>
         <div className={sHeroFooter.callback}>
           <CallbackOpenModalButton>
--- src/widget/header/ui/header.module.tsx
+++ src/widget/header/ui/header.module.tsx
@@ -3,14 +3,16 @@ import { FC, HTMLAttributes } from "react";
 import { Action } from "./action/action";
 import { ContactHeader } from "./contact/contact.header";
 import { HeaderLayout } from "./layout/header.layout";
+import { useLocale } from "next-intl";
 
 interface HeaderModuleProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const HeaderModule: FC<HeaderModuleProps> = (props) => {
   const { className } = props;
+  const locale = useLocale();
   return (
     <HeaderLayout
-      VisualSlot={<LogoLink href="/" view="PRIMARY" />}
+      VisualSlot={<LogoLink href={`/${locale}`} view="PRIMARY" />}
       InfoSlot={<ContactHeader />}
       ActionSlot={<Action />}
       className={className}
